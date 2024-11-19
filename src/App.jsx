c09a6f20c1cf7a441869921b5fabfb774b2a27d6
Enyewashebr <Enyewashebir0@gmail.com>
2024-11-19 15:00:30 +0300

    Add router and image icons to sidebar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
     </div>
-  )
-}
+  );}
 
 export default App
