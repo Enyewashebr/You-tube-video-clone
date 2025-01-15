baa993f2c34e420d37546f7d33c620a705057fa0
Enyewashebr <Enyewashebir0@gmail.com>
2025-01-15 18:54:44 +0300

    fetching data

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,163 +1,63 @@
-// import React from 'react'
-import './Feed.css'
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
+import { useEffect, useState } from "react";
+import "./Feed.css";
 import { Link } from "react-router-dom";
-const Feed = ({category}) => {
-  
+import { API_KEY } from "../../data";
+import {value_converter} from '../../data'
+
+
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
+
+  // Corrected function name and added error handling
+  const fetchData = async () => {
+    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+    try {
+      const response = await fetch(videoList_url);
+      const result = await response.json();
+      setData(result.items || []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [category]);
+
   return (
-    <Link to={`video/20/4251`} className="feed">
-      <div className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
+    <div className="feed">
+      {data.map((item) => {
+        const { id, snippet, statistics } = item;
+        const { title, channelTitle, thumbnails, publishedAt } = snippet;
 
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
+        // Format views and date (optional)
+        // const views = statistics?.viewCount || "N/A";
+        // const date = new Date(publishedAt).toLocaleDateString();
 
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a wbe developer
-        </h2>
-        <h3>GreateStack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-    </Link>
+        return (
+          <Link
+            to={`/video/${item.snippet.categoryId}/${item.id}`}
+            key={id}
+            className="feed-item"
+          >
+            <div className="card">
+              <img
+                src={item.snippet.thumbnails.medium.url}
+                alt={title}
+                className="thumbnail"
+              />
+              <h2 className="video-title">{item.snippet.title}</h2>
+              <h3 className="channel-name">{channelTitle}</h3>
+              <p className="video-stats">
+                {value_converter(item.statistics.viewCount)} views &bull;{" "}
+                {moment(item.snippet.publishedAt).fromNow()}
+              </p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
   );
-}
+};
 
-export default Feed
+export default Feed;
--- src/data.js
+++ src/data.js
@@ -1,1 +1,14 @@
 export const API_KEY = "AIzaSyBCGBNmEAG3EfzA_hwX5YKF2zfhw2dF4Z0";
+
+export const value_converter = (value) => {
+    if (value>=1000000) {
+        return Math.floor(value/1000000) + 'M';
+    }
+    else if (value >=1000) {
+        return Math.floor(value/1000) + 'K';
+
+    }
+    else {
+        return value
+    }
+}
